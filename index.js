ce7e7ff51d0e527caa79a0276c6e38a8345c8e04
Naizu <nrjofre@miuandes.cl>
2022-10-09 22:45:29 -0300

    add cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const User = require('./config');
 const app = express();
 
 const PORT = (process.env.PORT || 8080)
 
 app.use( express.json() );
+app.use( cors() );
 
 app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`));
 
