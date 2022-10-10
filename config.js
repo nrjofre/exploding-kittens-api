855c31198f7b85430a7151222e91136e80f7ca37
Naizu <nrjofre@miuandes.cl>
2022-10-09 22:09:07 -0300

    initial commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const firebase = require("firebase");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDQJNk4KB8eNEzSq5TFpqo_V3Wf9cSHg10",
+  authDomain: "exploding-kittens-api.firebaseapp.com",
+  projectId: "exploding-kittens-api",
+  storageBucket: "exploding-kittens-api.appspot.com",
+  messagingSenderId: "59881806540",
+  appId: "1:59881806540:web:8ebc6aeda59a1821fe8069",
+  measurementId: "G-TL7MK2Q5JG"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const User = db.collection("Users");
+module.exports = User;
---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const User = require('./config');
+const app = express();
+
+const PORT = 8080;
+
+app.use( express.json() );
+
+app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`));
+
+
+//get all users
+app.get('/users', async(req, res) => {
+    const snapshot = await User.get();
+    const list = snapshot.docs.map((doc) => ({ id:doc.id, ...doc.data() }));
+    res.send(list);
+});
+
+//register user
+app.post('/register', async(req, res) => {
+    const data = req.body;
+    await User.add(data);
+    res.send({msg: "User Registered"});    
+});
+
+//update user
+app.post('/update', async(req, res) => {
+    const id = req.body.id;
+    delete req.body.id;
+    const data = req.body;
+    await User.doc(id).update(data);
+    res.send({msg: "User Updated"});    
+});
+
+//delete user
+app.post('/delete', async(req, res) => {
+    const id = req.body.id;
+    await User.doc(id).delete();
+    res.send({msg: "User Deleted"});    
+});
