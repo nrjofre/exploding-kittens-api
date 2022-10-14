69281e4439df51b74385096faf4878539f379dc8
Naizu <nrjofre@miuandes.cl>
2022-10-13 22:03:22 -0300

    register change

--- index.js
+++ index.js
@@ -37,7 +37,9 @@ app.post('/login', async(req, res) => {
 
 //register user
 app.post('/register', async(req, res) => {
+    console.log("body");
     console.log(req.body);
+    console.log("body");
     const {email, username, password } = req.body;
     const data = {
                     "email": email,
