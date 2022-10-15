298abf578a221e6e80a6144dfe91127692ec8bd8
Naizu <nrjofre@miuandes.cl>
2022-10-15 16:03:18 -0300

    Change register and login

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.get('/users', async(req, res) => {
 
 //get friends
 app.get('/friends/:username', async(req, res) => {
+    console.log(req.params);
     const { username } = req.params;
     const snapshot = await User.get();
     const list = snapshot.docs.map((doc) => ({ id:doc.id, ...doc.data() }));
@@ -38,37 +39,45 @@ app.get('/friends/:username', async(req, res) => {
 app.post('/login', async(req, res) => {
     const data = req.body;
     console.log(req.body);
-    if (!data.email || !data.password){
-        return res.status(418).send({msg: "Need email and password"});
+    if (!data.username || !data.password){
+        return res.status(418).send({msg: "Need username and password"});
     }
 
-    const email = data.email;
+    const username = data.username;
     const password = data.password;
 
     const snapshot = await User.get();
     const list = snapshot.docs.map((doc) => ({ id:doc.id, ...doc.data() }));
 
     for (let i = 0; i < list.length; i++) {
-        if (list[i].email == email && list[i].password == password){
-            return res.send({msg: "User Acepted"});
+        if (list[i].username == username && list[i].password == password){
+            return res.send({id: list[i].id, username: list[i].username});
         }
     }
-    return res.status(400).send({msg: "Email does not match password"});
+    return res.status(400).send({msg: "Username and password do not match"});
 });
 
 //register user
 app.post('/register', async(req, res) => {
-    console.log("body");
     console.log(req.body);
-    console.log("body");
     const data = req.body;
 
     await User.add(data);
-    return res.send({msg: "User Registered"});    
+
+    const username = data.username;
+    const snapshot = await User.get();
+    const list = snapshot.docs.map((doc) => ({ id:doc.id, ...doc.data() }));
+
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].username == username){
+            return res.send(list[i]);
+        }
+    }  
 });
 
 //update user
 app.post('/update', async(req, res) => {
+    console.log(req.body);
     const id = req.body.id;
     delete req.body.id;
     const data = req.body;
@@ -78,6 +87,7 @@ app.post('/update', async(req, res) => {
 
 //delete user
 app.post('/delete', async(req, res) => {
+    console.log(req.body);
     const id = req.body.id;
     await User.doc(id).delete();
     return res.send({msg: "User Deleted"});    
