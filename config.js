680006ba7e4b6328ae6bf63f65b2c0b68c8aeb9a
Naizu <nrjofre@miuandes.cl>
2022-10-24 10:29:59 -0300

    firebase quota

--- config.js
+++ config.js
@@ -1,24 +1,15 @@
 const firebase = require("firebase");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDQJNk4KB8eNEzSq5TFpqo_V3Wf9cSHg10",
-  authDomain: "exploding-kittens-api.firebaseapp.com",
-  projectId: "exploding-kittens-api",
-  storageBucket: "exploding-kittens-api.appspot.com",
-  messagingSenderId: "59881806540",
-  appId: "1:59881806540:web:8ebc6aeda59a1821fe8069",
-  measurementId: "G-TL7MK2Q5JG"
+  apiKey: "AIzaSyC7qKv-bAZS4Dpocw94rbCAsoFJi3LqIZY",
+  authDomain: "exploding-kittens-api2.firebaseapp.com",
+  projectId: "exploding-kittens-api2",
+  storageBucket: "exploding-kittens-api2.appspot.com",
+  messagingSenderId: "948235126096",
+  appId: "1:948235126096:web:804e836c026917dffc5855"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 module.exports = db;
-
-//const User = db.collection('Users');
-//const FriendInvite = db.collection('Friend Invites');
-//const MatchInvites = db.collection("Match Invites");
-
-
-//module.exports = FriendInvite;
-//module.exports = MatchInvites;
